a195f2595c014b27acfef2e680aeabc2e34263b1
Matthias Hinojosa <hinojosamatthias@gmail.com>
2024-05-22 10:57:15 -0500

    Created Task.java model

---
+++ src/main/java/com/project_t/task/models/Task.java
@@ -0,0 +1,48 @@
+package com.project_t.task.models;
+
+public class Task {
+  private long id;
+
+  private String title;
+
+  private String description;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Task() {
+  }
+
+  public Task(String title, String description) {
+    this.title = title;
+    this.description = description;
+  }
+
+  // public Task(long id, String title, String description) {
+  // this.id = id;
+  // this.title = title;
+  // this.description = description;
+  // }
+
+}
