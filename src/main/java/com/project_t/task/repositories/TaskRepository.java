f1dc49fa3b89d7f507efeb19e3c733f6c33f0f32
Matthias Hinojosa <hinojosamatthias@gmail.com>
2024-05-22 12:34:30 -0500

    Added findByTitle method to TaskRepo

--- src/main/java/com/project_t/task/repositories/TaskRepository.java
+++ src/main/java/com/project_t/task/repositories/TaskRepository.java
@@ -1,10 +1,11 @@
 package com.project_t.task.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project_t.task.models.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long>{
-
-
+  List<Task> findByTitle(String title);
 }
