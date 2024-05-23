72ce3136f7b240c11d77bb51395ea6a600e2a41e
Matthias Hinojosa <hinojosamatthias@gmail.com>
2024-05-23 10:31:23 -0500

    Working on many to many for categories

---
+++ src/main/java/com/project_t/task/models/Category.java
@@ -0,0 +1,43 @@
+package com.project_t.task.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(nullable = false, length = 50)
+  private String name;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Category() {
+  }
+
+  public Category(String name) {
+    this.name = name;
+  }
+
+}
