4003ae295f35dfc8069335526f70b97154b0b3a2
Matthias Hinojosa <hinojosamatthias@gmail.com>
2024-05-23 10:42:21 -0500

    Created category repo w/ method

---
+++ src/main/java/com/project_t/task/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.project_t.task.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project_t.task.models.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+  Category findCategoryByName(String name);
+}
