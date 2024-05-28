4e79876db1b1207c363000bad211afacc86e19f4
Matthias Hinojosa <hinojosamatthias@gmail.com>
2024-05-28 10:36:36 -0500

    Added method for changing email

--- src/main/java/com/project_t/task/controllers/UserController.java
+++ src/main/java/com/project_t/task/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.project_t.task.controllers;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project_t.task.models.User;
 import com.project_t.task.repositories.UserRepository;
@@ -21,4 +23,13 @@ public class UserController {
     return "profile";
   }
 
+  @PostMapping("/profile")
+  public String changeProfile(@RequestParam(name = "email") String email) {
+    User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    User user = userDao.findUserById(loggedInUser.getId());
+    user.setEmail(email);
+    userDao.save(user);
+    loggedInUser.setEmail(email);
+    return "redirect:/profile";
+  }
 }
