43054594abeb4ecc03de677b5d0e33fd85d2fef6
Matthias Hinojosa <hinojosamatthias@gmail.com>
2024-06-02 11:51:46 -0500

    Created securityContext for object-user

--- src/main/java/com/project_t/task/utils/Input.java
+++ src/main/java/com/project_t/task/utils/Input.java
@@ -1,5 +1,10 @@
 package com.project_t.task.utils;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+
 public class Input {
-  
+  public static Object isSomeoneLoggedIn(){
+    Object user = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    return user;
+  }
 }
