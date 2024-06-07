94abe2e95590694b0ea6fa12ed23b7821ae8e621
Matthias Hinojosa <hinojosamatthias@gmail.com>
2024-06-07 12:03:44 -0500

    Added date formatting for due date

--- src/main/resources/static/js/formatDate.js
+++ src/main/resources/static/js/formatDate.js
@@ -20,3 +20,23 @@ document.getElementById("publishAgain");
 let newPublishFormat = document.createTextNode(formatDate);
 getSpaceToInputNewPublish.appendChild(newPublishFormat);
 // https://stackoverflow.com/questions/41764061/adding-text-to-an-existing-text-element-in-javascript-via-dom
+
+
+let grabCurrentPublishFormat2 =
+document.getElementById("publish2").textContent;
+let changeCurrentPublishFormat2 = new Date(grabCurrentPublishFormat2);
+let newFormat2 = {
+day: "2-digit",
+month: "short",
+year: "numeric",
+};
+let formatDate2 = changeCurrentPublishFormat2.toLocaleString(
+"en-gb",
+newFormat2
+);
+
+// console.log("vvvvv: " + formatDate);
+let getSpaceToInputNewPublish2 =
+document.getElementById("publishAgain2");
+let newPublishFormat2 = document.createTextNode(formatDate2);
+getSpaceToInputNewPublish2.appendChild(newPublishFormat2);
