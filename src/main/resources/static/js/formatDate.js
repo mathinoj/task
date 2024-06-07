5cfad8a6e10dd52b2e608acab4ca2f161296949e
Matthias Hinojosa <hinojosamatthias@gmail.com>
2024-06-07 14:31:08 -0500

    Fixed date subtraction by adding UTC

--- src/main/resources/static/js/formatDate.js
+++ src/main/resources/static/js/formatDate.js
@@ -8,8 +8,8 @@ day: "2-digit",
 month: "short",
 year: "numeric",
 };
-let formatDate = changeCurrentPublishFormat.toLocaleString(
-"en-gb",
+let formatDate = changeCurrentPublishFormat.toLocaleDateString(
+"en-US", {timeZone: 'UTC'},
 newFormat
 );
 // https://blog.enterprisedna.co/how-to-convert-string-to-date-in-javascript-in-dd-mmm-yyyy-format/#:~:text=You%20can%20convert%20strings%20to,or%20the%20date%2Dfns%20libraries.
@@ -22,21 +22,21 @@ getSpaceToInputNewPublish.appendChild(newPublishFormat);
 // https://stackoverflow.com/questions/41764061/adding-text-to-an-existing-text-element-in-javascript-via-dom
 
 
-let grabCurrentPublishFormat2 =
-document.getElementById("publish2").textContent;
-let changeCurrentPublishFormat2 = new Date(grabCurrentPublishFormat2);
-let newFormat2 = {
+let grabCurrentTaskFormat2 =
+document.getElementById("publishTask").textContent;
+let changeCurrentTaskFormat2 = new Date(grabCurrentTaskFormat2);
+let newTaskFormat = {
 day: "2-digit",
 month: "short",
 year: "numeric",
 };
-let formatDate2 = changeCurrentPublishFormat2.toLocaleString(
-"en-gb",
-newFormat2
+let formatTaskDate = changeCurrentTaskFormat2.toLocaleDateString(
+"en-US", {timeZone: 'UTC'},
+newTaskFormat
 );
+// https://stackoverflow.com/questions/32877278/tolocaledatestring-is-subtracting-a-day
 
-// console.log("vvvvv: " + formatDate);
-let getSpaceToInputNewPublish2 =
-document.getElementById("publishAgain2");
-let newPublishFormat2 = document.createTextNode(formatDate2);
-getSpaceToInputNewPublish2.appendChild(newPublishFormat2);
+let getSpaceToInputNewTaskDate =
+document.getElementById("publishNewTask");
+let newTaskFormatSet = document.createTextNode(formatTaskDate);
+getSpaceToInputNewTaskDate.appendChild(newTaskFormatSet);
